d6083ed95c642f13708091323b03bae332146782
Duby67 <Duby67@yandex.ru>
2026-02-23 18:26:32 +0300

    Add tg start message

--- tokens.py
+++ tokens.py
@@ -3,10 +3,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-YANDEX_MUSIC_TOKEN = os.getenv("YANDEX_MUSIC_TOKEN")
+ADMIN_ID_STR=os.getenv("ADMIN_ID")
+if ADMIN_ID_STR is None:
+    raise ValueError("ADMIN_ID не найден в .env файле")
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN не найден в .env!")
+
+YANDEX_MUSIC_TOKEN = os.getenv("YANDEX_MUSIC_TOKEN")
 if not YANDEX_MUSIC_TOKEN:
     raise ValueError("YANDEX_MUSIC_TOKEN не найден в .env!")
+
+ADMIN_ID = int(ADMIN_ID_STR)
