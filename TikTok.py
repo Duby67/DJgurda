6afd40024c6222c631c49e9fe173ce4fec8b65c9
Duby67 <Duby67@yandex.ru>
2026-02-23 23:51:33 +0300

    Update REGEX

--- TikTok.py
+++ TikTok.py
@@ -10,9 +10,7 @@ from base_handler import BaseHandler
 logger = logging.getLogger(__name__)
 
 class TikTokHandler(BaseHandler):
-    PATTERN = re.compile(
-        r'https?://(?:www\.|vm\.)?tiktok\.com/(?:@[\w.-]+/video/\d+|[\d]+|[a-zA-Z0-9_-]+)'
-    )
+    PATTERN = re.compile(r'https?://(?:www\.|vm\.)?tiktok\.com/(?:@[\w.-]+/video/\d+|[\d]+|[a-zA-Z0-9_-]+)(?:\?[^\s]*)?')
     TEMP_DIR = Path("temp_files/TikTok")
     TEMP_DIR.mkdir(parents=True, exist_ok=True)
 
