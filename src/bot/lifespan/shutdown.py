8f46495ea9a0c96b7a942c0cb67e34231f5c18f4
unknown <User@DESKTOP-DUBY67>
2026-02-26 20:53:29 +0300

    FIX dispatcher on lifespan

--- src/bot/lifespan/shutdown.py
+++ src/bot/lifespan/shutdown.py
@@ -1,12 +1,12 @@
 import logging
-from aiogram import Dispatcher
+from aiogram import Bot, Dispatcher
 
 from src.config import ADMIN_ID
 logger = logging.getLogger(__name__)
 
-async def on_shutdown(dispatcher: Dispatcher) -> None:
+async def on_shutdown(bot: Bot, dispatcher: Dispatcher) -> None:
     logger.info("Бот останавливается...")
     try:
-        await dispatcher.bot.send_message(chat_id=ADMIN_ID, text="⚠️ Бот выключается...")
+        await bot.send_message(chat_id=ADMIN_ID, text="⚠️ Бот выключается...")
     except Exception as e:
         logger.error(f"Не удалось отправить уведомление о выключении: {e}")
--- src/bot/lifespan/startup.py
+++ src/bot/lifespan/startup.py
@@ -1,14 +1,14 @@
 import time
 import logging
 
-from aiogram import Dispatcher
+from aiogram import Bot
 from zoneinfo import ZoneInfo
 from datetime import datetime, timezone
 
 from src.config import ADMIN_ID, PROJECT_TEMP_DIR, MAX_AGE_SECONDS
 logger = logging.getLogger(__name__)
 
-async def on_startup(dispatcher: Dispatcher) -> None:
+async def on_startup(bot: Bot) -> None:
     now = time.time()
     for f in PROJECT_TEMP_DIR.glob("**/*"):
         if f.is_file() and (now - f.stat().st_mtime) > MAX_AGE_SECONDS:
@@ -16,9 +16,9 @@ async def on_startup(dispatcher: Dispatcher) -> None:
 
     utc_time = datetime.now(timezone.utc)
     moscow_tz = ZoneInfo("Europe/Moscow")
-    dispatcher.bot["start_time"] = utc_time.astimezone(moscow_tz)
+    bot["start_time"] = utc_time.astimezone(moscow_tz)
     logger.info("Бот запущен")
     try:
-        await dispatcher.bot.send_message(chat_id=ADMIN_ID, text="✅ Бот успешно запущен и готов к работе!")
+        await bot.send_message(chat_id=ADMIN_ID, text="✅ Бот успешно запущен и готов к работе!")
     except Exception as e:
         logger.error(f"Не удалось отправить уведомление о запуске: {e}")
