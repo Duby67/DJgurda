0f68a1906b9e11e5110f4585e51e7d2f7c7213dd
unknown <User@DESKTOP-DUBY67>
2026-02-26 20:16:34 +0300

    Update startup clean old files

--- src/bot/lifespan/startup.py
+++ src/bot/lifespan/startup.py
@@ -1,13 +1,19 @@
+import time
 import logging
-from aiogram import Dispatcher
 
+from aiogram import Dispatcher
 from zoneinfo import ZoneInfo
 from datetime import datetime, timezone
 
-from src.config import ADMIN_ID
+from src.config import ADMIN_ID, PROJECT_TEMP_DIR, MAX_AGE_SECONDS
 logger = logging.getLogger(__name__)
 
 async def on_startup(dispatcher: Dispatcher) -> None:
+    now = time.time()
+    for f in PROJECT_TEMP_DIR.glob("**/*"):
+        if f.is_file() and (now - f.stat().st_mtime) > MAX_AGE_SECONDS:
+            f.unlink()
+
     utc_time = datetime.now(timezone.utc)
     moscow_tz = ZoneInfo("Europe/Moscow")
     dispatcher.bot["start_time"] = utc_time.astimezone(moscow_tz)
